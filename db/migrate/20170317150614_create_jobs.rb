7d98f4c338d7fc42e17474790a049fc3c2f61fc5
Moritz Breit <mail@moritz-breit.de>
2017-03-21 08:56:08 +0100

    Add database index

--- db/migrate/20170317150614_create_jobs.rb
+++ db/migrate/20170317150614_create_jobs.rb
@@ -9,5 +9,8 @@ class CreateJobs < ActiveRecord::Migration[5.0]
       t.timestamp :scheduled_for
       t.timestamp :performed_at
     end
+    add_index :pg_jobs, %i(queue_name scheduled_for priority created_at),
+              where: 'performed_at IS NULL',
+              name: 'index_pg_jobs_worker'
   end
 end
